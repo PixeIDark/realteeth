8b2babcf60a9140dac7baec818995d732ce37af4
PixeIDark <kiki7951@naver.com>
2026-01-24 18:51:54 +0900

    chore: style파일 경로 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes/router.tsx";
 import { QueryProvider } from "./providers/QueryProvider.tsx";
+import "./style/global.css";
 
 function App() {
   return (
