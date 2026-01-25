b81fb4e2edd73b5c476fff7a2ea21ffd496cfb1e
PixeIDark <kiki7951@naver.com>
2026-01-25 11:27:38 +0900

    style: css 속성 제거

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -8,7 +8,7 @@ function HomePage() {
   const { location } = useCurrentLocation();
 
   return (
-    <div className="mb-6 flex flex-col gap-6 sm:gap-8">
+    <div className="flex flex-col gap-6 sm:gap-8">
       <DistrictSearchBox />
       <WeatherCard lat={location.lat} lon={location.lon} />
       <HourlyForecastCard lat={location.lat} lon={location.lon} />
